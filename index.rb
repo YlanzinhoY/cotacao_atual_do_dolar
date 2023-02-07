c63012e38770fc33bc7343711da715a42e0be50e
enzinho <enzo.paxpayne@gmail.com>
2023-02-07 13:47:23 -0300

    transformando em classe

--- index.rb
+++ index.rb
@@ -1,7 +1,13 @@
 require 'rest-client'
 require 'json'
 
-req = RestClient.get 'https://economia.awesomeapi.com.br/last/USD'
-@response = JSON.parse(req.body)["USDBRL"]['bid']
+class Cotacao
+  def consumir_api
+    req = RestClient.get 'https://economia.awesomeapi.com.br/last/USD'
+    @response = JSON.parse(req.body)["USDBRL"]['bid']
+    puts("A cotação atual do dolar é: 💲#{sprintf("%.2f", @response)} Reais💰")
+  end
+end
+cotacao = Cotacao.new
+cotacao.consumir_api
 
-puts("A cotação atual do dolar é: 💲#{sprintf("%.2f", @response)} Reais💰")
